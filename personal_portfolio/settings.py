1da6a100a1d7e75c4bf0c6d470f93c9a374bb501
olawa001 <abdullateefyusuf80@gmail.com>
2024-07-26 16:55:11 +0100

    common

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -116,11 +116,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-MEDIA_ROOT = BASE_DIR / "uploads/"
-MEDIA_URL = '/media/'
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
